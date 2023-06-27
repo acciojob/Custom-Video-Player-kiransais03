6331a73e80a0ac340b255e17297643bb09a9dbe0
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-27 14:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 /* Edit this file */
 const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
+const videoElem = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
+const playButton = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+
+playButton.addEventListener("click", handlePlayButton, false);
+playVideo();
+
+async function playVideo() {
+  try {
+    await videoElem.play();
+    playButton.classList.add("playing");
+  } catch (err) {
+    playButton.classList.remove("playing");
+  }
+}
+
+function handlePlayButton() {
+  if (videoElem.paused) {
+    playVideo();
+  } else {
+    videoElem.pause();
+    playButton.classList.remove("playing");
+  }
+}
